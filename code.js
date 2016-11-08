5b92e66fff7027da6494efb0a4631188530bd30c
Wentian Ma <bj@bjdeMacBook-Air.local>
2016-11-08 14:34:15 -0500

    change file name

--- code.js
+++ code.js
@@ -1,21 +1,21 @@
 /* functionality for interactive quiz
 
 user scroll down to the quiz section
-    computer pop up window: prompt ("Now you know the differences between good and bad interview-body langauges, Let's give it try in this fun quiz! Select a picture by clicking it and submit the answer.")
+    computer prompt ("Now you know the differences between good and bad interview-body langauges, Let's give it try in this fun quiz! Select a picture by clicking it and submit the answer.")
 user close the window
 user click on pictures and submit
 if answers are correct
     computer alert users with success message ("Well done! You get it right.")
-    computer mark the next quetion button to active
+    computer mark the next quetion button active
     computer mark the question to true
 else if the number of selected pictures do not match with the indictated number
-    computer alter user ("Watch out! You need to select # answers")
+    computer alert user ("Watch out! You need to select # answers")
     computer mark the question to false
 else
     computer alert user ("Oh snap! Go over this part (link to the part) and try again")
     computer mark the question to false
 
-if all questions are marked into true
+if all questions are true
     computer alert user with a congratulation message ("Congratualation, you completed the quiz! Now you are ready to become an interview body langauge expert! ")
 
 */
