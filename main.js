32ab6376a547613b212386097ec008ad3b9146e4
Wentian Ma <bj@bjdeMacBook-Air.local>
2016-11-08 13:18:55 -0500

    add pseudocode

---
+++ main.js
@@ -0,0 +1,27 @@
+/* functionality for interactive quiz
+
+user scroll down to the quiz section
+    computer pop up window: prompt ("Now you know the differences between good and bad interview-body langauges, Let's give it try in this fun quiz! Select a picture by clicking it and submit the answer.")
+user click on pictures and submit
+if answers are correct
+    computer alert users with success message ("Well done! You get it right.")
+    computer mark the next quetion button to active
+else if the number of selected pictures do not match with the indictated number
+    computer alter user ("Watch out! You need to select # answers")
+else
+    computer alert user ("Oh snap! Go over this part (link to the part) and try again")
+
+*/
+
+
+
+/* functionality for self-evaluation checklist
+
+user checkoff items on check list and submit
+    computer pop up a window with a list of checkoffed items
+user click the download button
+    computer pop up a window for the download options
+user click the close button
+    computer return to the homepage
+
+*/
